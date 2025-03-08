5024f7eadb29c1c96c747f2a9ed92c55ad34f7c3
opensh <125738740+openMJJ@users.noreply.github.com>
2025-03-08 13:57:09 +0800

    Update install-theme.sh

--- script/install-theme.sh
+++ script/install-theme.sh
@@ -5,7 +5,7 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-GITHUB_RAW_URL="raw.githubusercontent.com/chenx-dust/nezha-compat/compat"
+GITHUB_RAW_URL="raw.githubusercontent.com/openMJJ/nezha-compat/compat"
 NZ_DASHBOARD_PATH="/dashboard"
 
 _repo=$1
